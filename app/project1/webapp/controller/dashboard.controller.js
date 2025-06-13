7597294dc7aef5cba0b7e1028382b28ffc9cc22f
Srilakshmanan M <sri22092001@gmail.com>
2025-06-13 10:43:46 +0000

    repushing role based dashboard login

--- app/project1/webapp/controller/dashboard.controller.js
+++ app/project1/webapp/controller/dashboard.controller.js
@@ -3,46 +3,11 @@ sap.ui.define([
   "sap/m/MessageToast"
 ], function (Controller, MessageToast) {
   "use strict";
- 
+
   return Controller.extend("project1.controller.dashboard", {
     onInit: function () {
-<<<<<<< HEAD
-      const oModel = new sap.ui.model.json.JSONModel({
-        tiles: [
-          {
-            title: "Apply Loan",
-            description: "Apply for new education loan",
-            icon: "https://cdn-icons-png.flaticon.com/512/1828/1828817.png",
-            key: "ApplyLoan"
-          },
-          
-          {
-            title: "Profile",
-            description: "User details and information",
-            icon: "https://cdn-icons-png.flaticon.com/512/1077/1077063.png",
-            key: "Profile"
-          },
-          
-        ],
-
-        tiles1 : [
-          {
-            title: "Loan Status",
-            description: "Check current status of loan",
-            icon: "https://cdn-icons-png.flaticon.com/512/3135/3135773.png",
-            key: "LoanStatus"
-          },
-          {
-            title: "Applied Loans",
-            description: "Details of Customer Loan Applications",
-            icon: "https://cdn-icons-png.flaticon.com/512/943/943593.png",
-            key: "AppliedLoan"
-          }
-          
-        ]
-=======
       let oUserModel = sap.ui.getCore().getModel("UserModel");
- 
+
       if (!oUserModel) {
         const userData = localStorage.getItem("UserData");
         if (userData) {
@@ -51,55 +16,54 @@ sap.ui.define([
           sap.ui.getCore().setModel(oUserModel, "UserModel");
         }
       }
- 
+
       const userRole = oUserModel ? oUserModel.getProperty("/role") : "Customer";
       const isAdmin = userRole === "Admin";
- 
+
       const tiles = [
         {
           title: "Apply Loan",
           description: "Apply for new education loan",
-icon: "https://cdn-icons-png.flaticon.com/512/1828/1828817.png",
+          icon: "https://cdn-icons-png.flaticon.com/512/1828/1828817.png",
           key: "ApplyLoan"
         },
         {
           title: "Profile",
           description: "User details and information",
-icon: "https://cdn-icons-png.flaticon.com/512/1077/1077063.png",
+          icon: "https://cdn-icons-png.flaticon.com/512/1077/1077063.png",
           key: "Profile"
         }
       ];
- 
+
       const tiles1 = [
         {
           title: "Loan Status",
           description: "Check current status of loan",
-icon: "https://cdn-icons-png.flaticon.com/512/3135/3135773.png",
+          icon: "https://cdn-icons-png.flaticon.com/512/3135/3135773.png",
           key: "LoanStatus"
         }
       ];
- 
+
       if (isAdmin) {
         tiles1.push({
           title: "Applied Loans",
           description: "Details of Customer Loan Applications",
-icon: "https://cdn-icons-png.flaticon.com/512/943/943593.png",
+          icon: "https://cdn-icons-png.flaticon.com/512/943/943593.png",
           key: "AppliedLoan"
         });
       }
- 
+
       const oModel = new sap.ui.model.json.JSONModel({
         tiles: tiles,
         tiles1: tiles1
->>>>>>> 7cfa304 (Based on role dashboard will be visible)
       });
       this.getView().setModel(oModel);
     },
- 
+
     onTilePress: function (oEvent) {
       const key = oEvent.getSource().getContent()[0].getCustomData()[0].getValue();
       const router = sap.ui.core.UIComponent.getRouterFor(this);
- 
+
       switch (key) {
         case "ApplyLoan":
           router.navTo("RouteloanApplication");
@@ -114,16 +78,16 @@ icon: "https://cdn-icons-png.flaticon.com/512/943/943593.png",
           router.navTo("AdminAppliedLoans");
           break;
         default:
-MessageToast.show("Unknown tile key: " + key);
+          MessageToast.show("Unknown tile key: " + key);
       }
     },
- 
+
     onLogout: function () {
       sap.ui.getCore().setModel(null, "UserModel");
       localStorage.removeItem("UserData");
       var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
       oRouter.navTo("RouteView1");
-MessageToast.show("Logged out!");
+      MessageToast.show("Logged out!");
     }
   });
 });
